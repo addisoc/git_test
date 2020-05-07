0719a10e4b7e92d72b4dc45fdbef428f8ff09cfe
Addison Chung <addisoc13@gmail.com>
2020-05-07 10:04:42 +0200

    [ALL][Feature] STRY928 - Add support for release notes

--- test.sh
+++ test.sh
@@ -3,4 +3,13 @@
 #It will discard prints oF automatic Merges and Pull Requests commits. 
 #It will show all the Commits date wise and sorted
 
-git log $1..$2 --pretty=format:%s
+echo "Features"
+
+git log $1..$2 --pretty=format:%s | grep -i -E "\[ALL\]|\[$3\]" | grep "Feature"
+
+echo "Fixes"
+git log $1..$2 --pretty=format:%s | grep -i -E "\[ALL\]|\[$3\]" | grep "Fix"
+
+
+echo "Misc"
+git log $1..$2 --pretty=format:%s | grep -i -E "\[ALL\]|\[$3\]" | grep "Misc"
